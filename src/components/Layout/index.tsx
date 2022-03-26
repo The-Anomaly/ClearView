c21c8c6ff97c14ead410683de8853dc73206e16f
Favour Adekogbe <favouradekogbe@gmail.com>
2022-03-26 21:57:00 +0100

    layout setup - navbar

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Navbar } from "./subComponents";
+
+const Layout = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+};
+
+export { Layout };
+export * from "./subComponents";
---
+++ src/components/Layout/subComponents/Navbar/index.tsx
@@ -0,0 +1,27 @@
+import { ClearViewWhite } from "assets";
+import { Button } from "components/subComponents";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+
+const Navbar = () => {
+  return (
+    <section className={`appContainer`}>
+      <div className={styles.navWrapper}>
+        <ClearViewWhite className={styles.navLogo} title="clear view" />
+        <nav className={styles.navList}>
+          <Link to="/">Home</Link>
+          <Link to="/">About us</Link>
+          <Link to="/">Contact us</Link>
+        </nav>
+        <Button
+          text="Contact us"
+          type="green"
+          onClick={() => console.log("contact us")}
+        />
+      </div>
+    </section>
+  );
+};
+
+export { Navbar };
