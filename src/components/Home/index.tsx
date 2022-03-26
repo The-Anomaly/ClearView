e48ee7cad9048b61a87afdebacccce7fb2617dd0
Favour Adekogbe <favouradekogbe@gmail.com>
2022-03-26 20:30:04 +0100

    app setup

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import styles from "./styles.module.css";
+
+const HomeUI = () => {
+  return (
+    <>
+      <div>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+      </div>
+    </>
+  );
+};
+
+export { HomeUI };
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import { HomeUI } from "components";
+import * as React from "react";
+
+const Home = () => {
+  return <HomeUI />;
+};
+
+export { Home };
